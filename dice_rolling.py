f50c2f865cf05055914683c226e803235b74eb0e
TheGodlessOne <alexnikv@mail.ru>
2020-02-19 19:38:26 +0300

    final

--- dice_rolling.py
+++ dice_rolling.py
@@ -5,19 +5,18 @@ class Game:
         self.msg = {
                     'greet': "Hello, my fellow friend! Do you throw the dices? y/n ",
                     'quest': "Excellent!",
-                    'throws': "Throw #{0}: {1} {2}",
+                    'throws': "Throw #{0}: {1}",
                     'restart': "Do you want to play more? y/n ",
                     'farewell': "So, good luck! "
                     }
+        self.num_of_games = 0
         
     def Dices(self, a=48271, c=0, m=31): 
-        t = [math.ceil(float(time.monotonic()))]
-        for i in range(55):
-            t.append(int((a*t[i] + c) % (math.pow(2, m)-1)))
-        print(t)
-        t.append(int((t[len(t)-55]+t[len(t)-24]) % math.pow(2, m)))
-        print(t)
-        return t
+        seq = [math.ceil(float(time.monotonic()))]
+        for i in range(2):
+            dice = int((a*seq[i] + c) % (math.pow(2,m) - 1))
+            seq.append(dice)
+            yield (dice % 6) + 1
 
     def main(self, invite='greet'): 
         q = input(self.msg.get(invite))
@@ -27,12 +26,14 @@ class Game:
             self.game_run()
 
     def game_run(self): 
+        self.num_of_games = self.num_of_games + 1
+        target = []
         print(self.msg.get('quest'))
-        i = 1
-        target = self.Dices()
-        print(self.msg.get('throws').format(i, target.next(), target.next()))
-        i = i + 1
+        for dice in self.Dices():
+            target.append(dice)
+        print(self.msg.get('throws').format(self.num_of_games, target))
         self.main('restart')
 
+
 game = Game(3)
-game.Dices()
+game.main()
