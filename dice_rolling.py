c5199c04fd3dd09f36da2e6f7c7d0ced3150c54c
TheGodlessOne <alexnikv@mail.ru>
2020-02-19 20:52:36 +0300

    complete test, probability close to real

--- dice_rolling.py
+++ dice_rolling.py
@@ -3,21 +3,23 @@ import time, math
 def test(game, iterations=50): 
     throw_list = []
     num_of_thrown = [0 for i in range(6)]
-    print(num_of_thrown)
+    #print(num_of_thrown)
     for i in range(iterations):
         for dice in game.Dices():
             throw = []
+            #print(dice)
             throw.append(dice)
-            num_of_thrown[dice] = num_of_thrown[dice] + 1
-            print(num_of_thrown)
+            num_of_thrown[dice-1] = num_of_thrown[dice-1] + 1
+            #print(num_of_thrown)
         throw_list.append(throw)
     print("Statistics:")
     for i in range(6):
-        print("{0} was thrown {1} times".format(i+1, num_of_thrown[i]))
+        print("{0} was thrown {1} times, probability: {2}%"
+            .format(i+1, num_of_thrown[i], 100*num_of_thrown[i]/iterations))
 
 
 class Game:
-    def __init__(self, lvl): 
+    def __init__(self, a=48271, c=0, m=31): 
         self.msg = {
                     'greet': "Hello, my fellow friend! Do you throw the dices? y/n ",
                     'quest': "Excellent!",
@@ -25,14 +27,17 @@ class Game:
                     'restart': "Do you want to play more? y/n ",
                     'farewell': "So, good luck! "
                     }
+        self.a = a
+        self.c = c
+        self.m = m
         self.num_of_games = 0
-        
-    def Dices(self, a=48271, c=0, m=31): 
-        seq = [math.ceil(float(time.monotonic()))]
+        self.seq = [math.ceil(float(time.monotonic()))]
         for i in range(55): 
-            seq.append(int((a*seq[i] + c) % (math.pow(2,m) - 1)))
-        dice = int((seq[len(seq)-55] + seq[len(seq)-24]) % math.pow(2,m))
-        seq.append(dice)
+            self.seq.append(int((self.a*self.seq[i] + self.c) % (math.pow(2,self.m) - 1)))
+        
+    def Dices(self): 
+        dice = int((self.seq[len(self.seq)-55] + self.seq[len(self.seq)-24]) % math.pow(2,self.m))
+        self.seq.append(dice)
         yield (dice % 6) + 1
 
     def main(self, invite='greet'): 
@@ -46,7 +51,7 @@ class Game:
         self.num_of_games = self.num_of_games + 1
         target = []
         print(self.msg.get('quest'))
-        for dice in range(2):
+        for dice in self.Dices():
             print(dice)
             target.append(dice)
         print(self.msg.get('throws').format(self.num_of_games, target))
@@ -55,4 +60,4 @@ class Game:
 
 game = Game(3)
 game.main()
-#test(game)
+#test(game, 10000)
