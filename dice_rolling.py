b0e3ca4e3b9191c0b1b90cad139a2b9d12e8c281
TheGodlessOne <alexnikv@mail.ru>
2020-02-19 18:39:41 +0300

    first attempt with random dices

--- dice_rolling.py
+++ dice_rolling.py
@@ -1,18 +1,20 @@
-import math
+import time, math
 
 class Game:
     def __init__(self, lvl): 
         self.msg = {
                     'greet': "Hello, my fellow friend! Do you throw the dices? y/n ",
-                    'quest': "Excellent! Hit 'Enter' for new throw, and ",
-                    'throws': "Throw #{0}: {1} {2}"
+                    'quest': "Excellent!",
+                    'throws': "Throw #{0}: {1} {2}",
+                    'restart': "Do you want to play more? y/n ",
                     'farewell': "So, good luck! "
                     }
         
-    def Dices(self):
-        (a, c, m) = (48271, 0, 6)
-        t = math.ceil(float(time.monotonic()))
-        yield (a * t + c) % m
+    def Dices(self, a=48271, c=0, m=6): 
+        t0 = math.ceil(float(time.monotonic()))
+        t = [(a*t0 + c) % m for i in range(55)]
+        t.append((t[len(t)-55]+t[len(t)-24]) % 2^m)
+        return t[len(t)]
 
     def main(self, invite='greet'): 
         q = input(self.msg.get(invite))
@@ -22,10 +24,12 @@ class Game:
             self.game_run()
 
     def game_run(self): 
+        print(self.msg.get('quest'))
         i = 1
         target = self.Dices()
-        print(self.msg.get('throws').format(i, target[0], target[1]))
-
+        print(self.msg.get('throws').format(i, target.next(), target.next()))
+        i = i + 1
+        self.main('restart')
 
 game = Game(3)
 game.game_run()
