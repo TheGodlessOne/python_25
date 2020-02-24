eb14f48f1502f1b53ff5267d5b3531e7d831e5dc
TheGodlessOne <alexnikv@mail.ru>
2020-02-24 21:37:22 +0300

    separated test print

--- binary_search.py
+++ binary_search.py
@@ -7,15 +7,23 @@ class BinarySearch:
 
     def search(self, x, search_type): 
         if search_type == 'rec': 
-            try: 
-                return self.recursive(x, self.left, self.right)
-            except IndexError: 
-                print("Value {0} is too small or too big".format(x))
+            while True:
+                try: 
+                    if (x<self.arr[0]or x>self.arr[-1]): raise IndexError
+                    return self.recursive(x, self.left, self.right)
+                    break
+                except IndexError: 
+                    print("Value {0} is too small or too big".format(x))
+                    break
         elif search_type == 'iter': 
-            try: 
-                return self.iterative(x, self.left, self.right)
-            except IndexError: 
-                print("Value {0} is too small or too big".format(x))
+            while True:
+                try: 
+                    if (x<self.arr[0]or x>self.arr[-1]): raise IndexError
+                    return self.iterative(x, self.left, self.right)
+                    break
+                except IndexError: 
+                    print("Value {0} is too small or too big".format(x))
+                    break
 
 
     def recursive(self, x, l, r): 
@@ -49,11 +57,12 @@ s_types = ['rec', 'iter']
 test_arr = [i for i in range(20)]
 print(test_arr)
 print(test_arr[-1])
-cases = [test_arr[0], 10, test_arr[-1], -1, 100]
+cases = [10, 10, test_arr[-1], -1, 100]
 test_search = BinarySearch(test_arr)
-for x in cases: 
-    print(x)
-    for s_type in s_types:
+for s_type in s_types:
+    print('Test the {0} alg'.format(s_type))
+    for x in cases: 
+        print(x)
         if test_search.search(x, s_type) is not None: 
             print("Value {0} has index {1} by {2} algorithm".format(x, test_search.search(x, s_type), s_type))
         else: 
