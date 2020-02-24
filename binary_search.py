5209328a9f4145bb93098c6c85986c3e4105fb2f
TheGodlessOne <alexnikv@mail.ru>
2020-02-24 21:01:34 +0300

    added tests and exceptions

--- binary_search.py
+++ binary_search.py
@@ -5,16 +5,24 @@ class BinarySearch:
         self.right = right 
 
 
+    def is_inbound(self): 
+        if x<self.arr[0]: 
+            raise IndexError
+        if x>self.arr[-1]: 
+            raise IndexError
+
+
     def search(self, x, search_type): 
         if search_type == 'rec': 
-            result = self.recursive(x)
+            try: 
+                return self.recursive(x)
+            except IndexError: 
+                print("Value {0} is too small or too big".format(x))
         elif search_type == 'iter': 
-            result = self.iterative(x)
-        
-        if result is None: 
-            print("There's no such element in array")
-        else: 
-            print("Element {0} is presented at {1} position by {2} algorithm".format(x, result, search_type))
+            try: 
+                return self.iterative(x)
+            except IndexError: 
+                print("Value {0} is too small or too big".format(x))
 
 
     def recursive(self, x): 
@@ -30,7 +38,7 @@ class BinarySearch:
                 return self.recursive(x) 
 
 
-    def iterative(self, x): 
+    def iterative(self, x):
         while self.left <= self.right: 
             middle = int(self.left + (self.right - self.left)/2) 
             if self.arr[middle] == x: 
@@ -42,7 +50,16 @@ class BinarySearch:
         return None
 
 
+s_types = ['rec', 'iter']
 test_arr = [i for i in range(20)]
+print(test_arr)
+print(test_arr[-1])
+cases = [test_arr[0], test_arr[int(len(test_arr)/2)], test_arr[-1], -1, 100]
 test_search = BinarySearch(test_arr, 0, len(test_arr))
-print(test_search.search(4, 'rec'))
-print(test_search.search(4, 'iter'))
+for x in cases: 
+    print(x)
+    for s_type in s_types:
+        if test_search.search(x, s_type) is None: 
+            print(test_search.search(x, s_type))
+        else: 
+            print("Value {0} has index {1} by {2} algorithm".format(x, test_search.search(x, s_type), s_type))
