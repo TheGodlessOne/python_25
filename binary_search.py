540a71ff8bf64bd6b8d65d5d44bb6137f1b27875
TheGodlessOne <alexnikv@mail.ru>
2020-02-24 21:52:03 +0300

    modified test cases in dict(comment, value) for more clear structure

--- binary_search.py
+++ binary_search.py
@@ -56,11 +56,20 @@ class BinarySearch:
 s_types = ['rec', 'iter']
 test_arr = [i*2 for i in range(20)]
 print(test_arr)
-cases = [test_arr[0], test_arr[-1], -1, 100]
+cases = {
+        'first element': test_arr[0],
+        'last element': test_arr[-1],
+        'lesser than minimum': -1,
+        'bigger than maximum': 100,
+        'not in the list, but in range': 15
+}
 test_search = BinarySearch(test_arr)
 for s_type in s_types:
     print('Test the {0} alg'.format(s_type))
-    for x in cases: 
-        # print(x)
+    for test_case in cases.items(): 
+        x = test_case[1]
+        print("Test case: {0}; test value: {1}".format(test_case[0], x))
         if test_search.search(x, s_type) is not None: 
             print("Value {0} has index {1} by {2} algorithm".format(x, test_search.search(x, s_type), s_type))
+        else: 
+            print("Value {0} is out of list".format(x))
