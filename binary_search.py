d07923b212c8029276a7c657d3f97542d32bf3ca
TheGodlessOne <alexnikv@mail.ru>
2020-02-24 21:22:23 +0300

    fixed problem with recursive boundaries

--- binary_search.py
+++ binary_search.py
@@ -1,52 +1,47 @@
 class BinarySearch:
-    def __init__(self, array, left, right): 
+    def __init__(self, array): 
         self.arr = array 
-        self.left = left 
-        self.right = right 
-
-
-    def is_inbound(self): 
-        if x<self.arr[0]: 
-            raise IndexError
-        if x>self.arr[-1]: 
-            raise IndexError
+        self.left = self.arr[0] 
+        self.right = self.arr[0] 
 
 
     def search(self, x, search_type): 
         if search_type == 'rec': 
             try: 
-                return self.recursive(x)
+                return self.recursive(x, self.left, self.right)
             except IndexError: 
                 print("Value {0} is too small or too big".format(x))
         elif search_type == 'iter': 
             try: 
-                return self.iterative(x)
+                return self.iterative(x, self.left, self.right)
             except IndexError: 
                 print("Value {0} is too small or too big".format(x))
 
 
-    def recursive(self, x): 
-        if self.right >= self.left: 
-            middle = int(self.left + (self.right - self.left)/2)
-            if self.arr[middle] == x: 
-                return middle 
-            elif self.arr[middle] > x: 
-                self.right = middle - 1
-                return self.recursive(x) 
+    def recursive(self, x, l, r): 
+        if r >= l: 
+            m = int(l + (r - l)/2)
+            if self.arr[m] == x: 
+                return m 
+            elif self.arr[m] > x: 
+                r = m - 1
+                return self.recursive(x, l, r) 
             else: 
-                self.left = middle + 1
-                return self.recursive(x) 
+                l = m + 1
+                return self.recursive(x, l, r) 
+        else: 
+            return None
 
 
-    def iterative(self, x):
-        while self.left <= self.right: 
-            middle = int(self.left + (self.right - self.left)/2) 
-            if self.arr[middle] == x: 
-                return middle 
-            elif self.arr[middle] < x: 
-                self.left = middle + 1
+    def iterative(self, x, l, r):
+        while l <= r: 
+            m = int(l + (r - l)/2) 
+            if self.arr[m] == x: 
+                return m
+            elif self.arr[m] < x: 
+                l = m + 1
             else: 
-                self.right = middle - 1
+                r = m - 1
         return None
 
 
@@ -54,12 +49,12 @@ s_types = ['rec', 'iter']
 test_arr = [i for i in range(20)]
 print(test_arr)
 print(test_arr[-1])
-cases = [test_arr[0], test_arr[int(len(test_arr)/2)], test_arr[-1], -1, 100]
-test_search = BinarySearch(test_arr, 0, len(test_arr))
+cases = [test_arr[0], 10, test_arr[-1], -1, 100]
+test_search = BinarySearch(test_arr)
 for x in cases: 
     print(x)
     for s_type in s_types:
-        if test_search.search(x, s_type) is None: 
-            print(test_search.search(x, s_type))
-        else: 
+        if test_search.search(x, s_type) is not None: 
             print("Value {0} has index {1} by {2} algorithm".format(x, test_search.search(x, s_type), s_type))
+        else: 
+            print('None')
