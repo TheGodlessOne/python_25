f47dcdbc820bfd5161d21b59cc423150ccd6bc2d
TheGodlessOne <alexnikv@mail.ru>
2020-02-27 02:31:23 +0300

    finished replacement of func-buttons

--- calculator.py
+++ calculator.py
@@ -2,20 +2,29 @@ from tkinter import *
 
 class Calc:
     def __init__(self, master):
-        self.func = {"+": None, "-": None, "*": None, "/": None}
-        self.num = {str(i): None for i in range(10)}
+        self.func = {"+": None, "-": None, "*": None, "/": None, "=": None}
+        self.num = {str(i+1): None for i in range(9)}
         self.entry = Entry(width = 10)
         self.entry.grid(row = 0, column = 0, columnspan = 4, sticky = W+E)
+        it=0
         for i in self.func.keys():
+            it += 1
             f = Button(master, text = "{}".format(i))
             self.func[i] = f
-            self.func[i].grid(row = 0, column = 4)
+            self.func[i].grid(row = it, column = 4)
         for i in self.num.keys():
-            n = Button(master, text = "{}".format(int(i)+1))
+            n = Button(master, text = "{}".format(int(i)))
             self.num[i] = n
-            self.num[i].grid(row = 4-int(i)//3, column = int(i)%3)
-        self.eq = Button(master, text = "=")
-        self.eq.grid(row = 5, column = 0, sticky = W+E)
+            self.num[i].grid(row = 4-(int(i)-1)//3, column = (int(i)-1)%3)
+        self.num.update({'0': None})
+        self.num['0'] = Button(master, text = "{}".format(0))
+        self.num['0'].grid(row = 5, column = 0)
+
+
+
+
+for i in range(10):
+    print('{0}//3 {1}, {0}%3 {2}'.format(i, i//3, i%3))
 
 root = Tk()
 calc = Calc(root)
