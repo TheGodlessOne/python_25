c90462c20aba1db606ffc193386b30fdd2afce49
TheGodlessOne <alexnikv@mail.ru>
2020-02-27 03:43:31 +0300

    binded num-buttons and entry

--- calculator.py
+++ calculator.py
@@ -2,10 +2,11 @@ from tkinter import *
 
 class Calc:
     def __init__(self, master):
-        self.x = self.y = None 
+        self.x = StringVar
+        self.y = StringVar
         self.func = {"+": None, "-": None, "*": None, "/": None, "=": None}
         self.num = {str(i+1): None for i in range(9)}
-        self.entry = Entry(width = 10)
+        self.entry = Entry(width = 10, justify=RIGHT)
         self.entry.grid(row = 0, column = 0, columnspan = 4, sticky = W+E)
         it=0
         for i in self.func.keys():
@@ -17,15 +18,15 @@ class Calc:
             n = Button(master, text = "{}".format(int(i)))
             self.num[i] = n
             self.num[i].grid(row = 4-(int(i)-1)//3, column = (int(i)-1)%3)
+            self.num[i].bind('<Button-1>', lambda event=int(i), num=i: self.number_action(event, num))
         self.num.update({'0': None})
         self.num['0'] = Button(master, text = "{}".format(0))
         self.num['0'].grid(row = 5, column = 0)
+        self.entry.delete(0, END)
+        self.entry.icursor(0)
 
-    def number_action():
-        
-
-    def func_action(): 
-
+    def number_action(self, event, num):
+        self.entry.insert(END, num)
 
 
 root = Tk()
