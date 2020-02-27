377172dda3c0f59ddb66c7beeb153e1b8969ec23
TheGodlessOne <alexnikv@mail.ru>
2020-02-27 05:41:27 +0300

    created the button dict to archive buttons and delete button

--- calculator.py
+++ calculator.py
@@ -9,18 +9,23 @@ class Calc:
         self.x = ''
         self.expression = ''
         self.func = ('+', '-', '*', '/', '=', 'C')
+        self.buttons = {}
 
         self.label = Label()
         self.label.grid(row = 0, column = 0, columnspan = 4, sticky = N+S+W+E)
 
-        self.entry = Entry(width = 10, justify=RIGHT)
+        self.entry = Entry(width = 10, text=0, justify=RIGHT)
         self.entry.grid(row = 1, column = 0, columnspan = 4, sticky = N+S+W+E)
         
         for n in range(10):
             self.add_button(master, str(n))
+            self.buttons.update({str(n): self.add_button(master, str(n))})
         
         for f in self.func: 
             self.add_button(master, f)
+            self.buttons.update({f: self.add_button(master,f)})
+
+        print(self.buttons)
 
         self.entry.delete(0, END)
         self.entry.icursor(0)
@@ -47,6 +52,7 @@ class Calc:
                             lambda event=label, func=label: self.func_action(event, func)
                             )
                 b.grid(row = self.func.index(label)+2, column = 3, sticky = N+S+W+E)
+        return b
 
     def number_action(self, event, num):
         self.entry.insert(END, num)
@@ -66,15 +72,18 @@ class Calc:
         self.entry.delete(0, END)
         self.label['text'] = self.expression + '=' + str(eval(self.expression))
         self.entry.insert(END, eval(self.expression))
+        self.expression = ''
         self.clear(event)
 
     def clear(self, event): 
-        self.expression = ''
-        self.entry.delete(0, END)
+        if self.buttons['C']['text'] == 'CE':
+            self.entry.delete(0, END)
+        elif self.buttons['C']['text'] == 'C': 
+            self.label['text'] = ''
+            self.expression = ''
+            self.entry.delete(0, END)
 
 
 root = Tk()
 calc = Calc(root)
-# Grid.rowconfigure(root, 0, weight=1)
-# Grid.columnconfigure(root, 0, weight=1)
 root.mainloop()
