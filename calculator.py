23939b664d07e0f75fa69b2ccbd5c0836d874b1c
TheGodlessOne <alexnikv@mail.ru>
2020-02-27 04:53:13 +0300

    one func to add all buttons

--- calculator.py
+++ calculator.py
@@ -4,37 +4,39 @@ class Calc:
     def __init__(self, master):
         self.x = ''
         self.expression = ''
-        self.func = {'+': None, '-': None, '*': None, '/': None}
-        self.num = {str(i+1): None for i in range(9)}
+        self.func = ('+', '-', '*', '/', '=')
+        # self.num = (str(i+1) for i in range(9))
         self.entry = Entry(width = 10, justify=RIGHT)
         self.entry.grid(row = 0, column = 0, columnspan = 4, sticky = W+E)
         
-        it=0
-        for i in self.func.keys():
-            it += 1
-            f = Button(master, text = '{}'.format(i))
-            self.func[i] = f
-            self.func[i].grid(row = it, column = 4)
-            self.func[i].bind('<Button-1>', lambda event=i, func=i: self.func_action(event, func))
+        for n in range(10):
+            self.add_button(master, str(n))
         
-        for i in self.num.keys():
-            n = Button(master, text = '{}'.format(int(i)))
-            self.num[i] = n
-            self.num[i].grid(row = 4-(int(i)-1)//3, column = (int(i)-1)%3)
-            self.num[i].bind('<Button-1>', lambda event=int(i), num=i: self.number_action(event, num))
-        
-        self.num.update({'0': None})
-        self.num['0'] = Button(master, text = '0')
-        self.num['0'].grid(row = 5, column = 0)
-        self.num['0'].bind('<Button-1>', lambda event=int('0'), num='0': self.number_action(event, num))
-        
-        self.func.update({'=': None})
-        self.func['='] = Button(master, text = '=')
-        self.func['='].grid(row = 5, column = 4)
-        self.func['='].bind('<Button-1>', self.equal)
+        for f in self.func: 
+            self.add_button(master, f)
 
         self.entry.delete(0, END)
         self.entry.icursor(0)
+        
+    def add_button(self, master, label): 
+        b = Button(master, text = '{}'.format(label))
+        if label.isnumeric() is True: 
+            b.bind('<Button-1>', 
+                        lambda event=int(label), num=label: self.number_action(event, num)
+                        )
+            if label == '0': 
+                b.grid(row = 5, column = 0)
+            else: 
+                b.grid(row = 4-(int(label)-1)//3, column = (int(label)-1)%3)
+        else: 
+            if label == '=': 
+                b.bind('<Button-1>', self.equal)
+            else: 
+                b.bind('<Button-1>', 
+                            lambda event=label, func=label: self.func_action(event, func)
+                            )
+            b.grid(row = self.func.index(label)+1, column = 4)
+
 
     def number_action(self, event, num):
         self.entry.insert(END, num)
